1cf16aa46586889cb3f2b65c45c43bf7c141c9e3
Princekumar maurya <mprincekumar91@gmail.com>
2020-04-03 03:59:09 +0530

    updated

--- app.py
+++ app.py
@@ -1,9 +1,71 @@
-from flask import Flask, render_template , flash, redirect, url_for, session
+from flask import Flask, render_template , flash, redirect, url_for, session, request
+from flask_sqlalchemy import flask_sqlalchemy
+from werkzeug import secure_filename
 
 app = Flask(__name__ , 
             static_url_path='', 
             static_folder='static')
 
+ENV = 'dev'
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:A4notebook@/nayamandi'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class RegisterBuyer(db.Model):
+    __tablename__ = 'register_buyer'
+    id = db.Column(db.Integer, primary_key=True)
+    regtype = db.Column(db.String(256))
+    fname = db.Column(db.String(256))
+    gender = db.Column(db.String(256))
+    Address = db.Column(db.String(256))
+    enterdate = db.Column(db.Integer)
+    pincode = db.Column(db.Integer)
+    state = db.Column(db.String(256))
+    distict = db.Column(db.String(256))
+    tehsil = db.Column(db.String(256))
+    photoid = db.Column(db.String(256))
+    idno = db.Column(db.String(256), unique = True)
+    mobno = db.Column(db.Integer, unique = True)
+    altmobno = db.Column(db.Integer)
+    emailaddr = db.Column(db.String(256), unique = True)
+    altemailaddr = db.Column(db.String(256))
+    bankname = db.Column(db.String(256))
+    holderbankname = db.Column(db.String(256))
+    bankaccno = db.Column(db.Integer, unique = True)
+    cbankaccno = db.Column(db.Integer, unique = True)
+    image=db.Column(db.String(256))
+    
+    def __init__(self, regtype, fname, gender, Address, enterdate, pincode, state, distict, tehsil, photoid, idno, mobno, altmobno, emailaddr, altemailaddr, bankname, holderbankname, bankaccno, cbankaccno, image):
+        self.regtype = regtype
+        self.fname = fname
+        self.gender = gender
+        self.Address = Address
+        self.enterdate = enterdate
+        self.pincode = pincode
+        self.state = state
+        self.distict = distict
+        self.tehsil = tehsil
+        self.photoid = photoid
+        self.idno = idno
+        self.mobno = mobno
+        self.altmobno = altmobno
+        self.emailaddr = emailaddr
+        self. altemailaddr = altemailaddr
+        self.bankname = bankname
+        self.holderbankname = holderbankname
+        self.bankaccno = bankaccno
+        self.cbankaccno = cbankaccno
+        self.image = image
+    
 app.config["DEBUG"] = True
 app.secret_key = 'super secret key'
 
@@ -63,6 +125,35 @@ def login_consumers():
 @app.route("/login_sellers")
 def login_sellers():
     return render_template('login_sellers.html')
+
+@app.route("/submit_buyer", methods=['GET','POST'])
+def submit():
+    if request.method == 'POST':
+        regtype = request.form['regtype']
+        fname = request.form['fname']
+        gender = request.form['gender']
+        Address = request.form['Address']
+        enterdate = request.form['enterdate']
+        pincode = request.form['pincode']
+        state = request.form['state']
+        distict = request.form['distict']
+        tehsil = request.form['tehsil']
+        photoid = request.form['photoid']
+        idno = request.form['idno']
+        mobno = request.form['mobno']
+        altmobno = request.form['altmobno']
+        emailaddr = request.form['emailaddr']
+        altemailaddr = request.form['altemailaddr']
+        bankname = request.form['bankname']
+        holderbankname = request.form['holderbankname']
+        bankaccno = request.form['bankaccno']
+        cbankaccno = request.form['cbankaccno']
+        photoidimg = request.files['sign']
+        photoidimg.save(secure_filename(photoidimg.filename))
+        print(regtype,fname,gender,Address,enterdate,pincode,state,distict)
+        print(tehsil,photoid,idno,mobno,altmobno,emailaddr,altemailaddr,bankname,holderbankname,bankaccno,cbankaccno)
+        print(photoidimg)
+        return "successfull"
 
 
 
