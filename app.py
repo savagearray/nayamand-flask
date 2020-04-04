691fb66d8024f1daef81e4a9893ebf226e1ef1cf
Princekumar maurya <mprincekumar91@gmail.com>
2020-04-04 21:46:25 +0530

    seller page connected to db

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__ ,
             static_url_path='', 
             static_folder='static')
 
-ENV = 'prod'
+ENV = 'dev'
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:A4notebook@localhost/nayamandi'
@@ -26,8 +26,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 
-class RegisterBuyer(db.Model):
-    __tablename__ = 'register_buyer'
+class RegisterConsumer(db.Model):
+    __tablename__ = 'register_consumer'
     id = db.Column(db.Integer, primary_key=True)
     regtype = db.Column(db.String(256))
     fname = db.Column(db.String(256))
@@ -50,7 +50,7 @@ class RegisterBuyer(db.Model):
     cbankaccno = db.Column(db.Integer, unique = True)
     
     
-    def __init__(self, regtype, fname, gender, Address, enterdate, pincode, state, distict, tehsil, photoid, idno, mobno, altmobno, emailaddr, altemailaddr, bankname, holderbankname, bankaccno, cbankaccno):
+    def __init__(self,regtype, fname, gender, Address, enterdate, pincode, state, distict, tehsil, photoid, idno, mobno, altmobno, emailaddr, altemailaddr, bankname, holderbankname, bankaccno, cbankaccno):
         self.regtype = regtype
         self.fname = fname
         self.gender = gender
@@ -72,6 +72,51 @@ class RegisterBuyer(db.Model):
         self.cbankaccno = cbankaccno
         
         
+class RegisterSeller(db.Model):
+    __tablename__ = 'register_seller'
+    id = db.Column(db.Integer, primary_key=True)
+    regtype = db.Column(db.String(256))
+    fname = db.Column(db.String(256))
+    gender = db.Column(db.String(256))
+    Address = db.Column(db.String(256))
+    enterdate = db.Column(db.String(256))
+    pincode = db.Column(db.Integer)
+    state = db.Column(db.String(256))
+    distict = db.Column(db.String(256))
+    tehsil = db.Column(db.String(256))
+    photoid = db.Column(db.String(256))
+    idno = db.Column(db.String(256), unique = True)
+    mobno = db.Column(db.Integer, unique = True)
+    altmobno = db.Column(db.Integer)
+    emailaddr = db.Column(db.String(256), unique = True)
+    altemailaddr = db.Column(db.String(256))
+    bankname = db.Column(db.String(256))
+    holderbankname = db.Column(db.String(256))
+    bankaccno = db.Column(db.Integer, unique = True)
+    cbankaccno = db.Column(db.Integer, unique = True)
+    
+    
+    def __init__(self,regtype, fname, gender, Address, enterdate, pincode, state, distict, tehsil, photoid, idno, mobno, altmobno, emailaddr, altemailaddr, bankname, holderbankname, bankaccno, cbankaccno):
+        self.regtype = regtype
+        self.fname = fname
+        self.gender = gender
+        self.Address = Address
+        self.enterdate = enterdate
+        self.pincode = pincode
+        self.state = state
+        self.distict = distict
+        self.tehsil = tehsil
+        self.photoid = photoid
+        self.idno = idno
+        self.mobno = mobno
+        self.altmobno = altmobno
+        self.emailaddr = emailaddr
+        self. altemailaddr = altemailaddr
+        self.bankname = bankname
+        self.holderbankname = holderbankname
+        self.bankaccno = bankaccno
+        self.cbankaccno = cbankaccno
+    
     
 app.config["DEBUG"] = True
 app.secret_key = 'super secret key'
@@ -117,6 +162,10 @@ def register_consumer():
 def register_seller():
     return render_template('registration_sellers.html')
 
+@app.route("/registration")
+def register_buyer():
+    return render_template('registration.html')
+
 @app.route("/about_us")
 def about_us():
     return render_template('about_us.html')
@@ -139,10 +188,10 @@ def login_sellers():
 
 
 
-@app.route("/submit_buyer", methods=['GET','POST'])
-def submit():
+@app.route("/register_consumer", methods=['GET','POST'])
+def submit_consumer():
     if request.method == 'POST':
-        regtype = request.form['regtype']
+        regtype = 'Consumer'
         fname = request.form['fname']
         gender = request.form['gender']
         Address = request.form['Address']
@@ -164,8 +213,8 @@ def submit():
         #photoidimg = request.files['sign']
         #photoidimg.save(secure_filename(photoidimg.filename))
        
-        if db.session.query(RegisterBuyer).filter(RegisterBuyer.mobno == mobno).count() == 0:
-            data = RegisterBuyer(regtype,fname,gender,Address,enterdate,pincode,state,distict,tehsil,photoid,idno,mobno,altmobno,emailaddr,altemailaddr,bankname,holderbankname,bankaccno,cbankaccno)
+        if db.session.query(RegisterConsumer).filter(RegisterConsumer.mobno == mobno).count() == 0:
+            data = RegisterConsumer(fname,regtype,gender,Address,enterdate,pincode,state,distict,tehsil,photoid,idno,mobno,altmobno,emailaddr,altemailaddr,bankname,holderbankname,bankaccno,cbankaccno)
             db.session.add(data)
             db.session.commit()
             return "submitted"
@@ -174,6 +223,40 @@ def submit():
         
         return "successfull"
 
+@app.route("/register_seller", methods=['GET','POST'])
+def submit_seller():
+    if request.method == 'POST':
+        regtype = 'Seller'
+        fname = request.form['fname']
+        gender = request.form['gender']
+        Address = request.form['Address']
+        enterdate = request.form['enterdate']
+        pincode = request.form['pincode']
+        state = request.form['state']
+        distict = request.form['distict']
+        tehsil = request.form['tehsil']
+        photoid = request.form['photoid']
+        idno = request.form['idno']
+        mobno = request.form['mobno']
+        altmobno = request.form['altmobno']
+        emailaddr = request.form['emailaddr']
+        altemailaddr = request.form['altemailaddr']
+        bankname = request.form['bankname']
+        holderbankname = request.form['holderbankname']
+        bankaccno = request.form['bankaccno']
+        cbankaccno = request.form['cbankaccno']
+        #photoidimg = request.files['sign']
+        #photoidimg.save(secure_filename(photoidimg.filename))
+       
+        if db.session.query(RegisterSeller).filter(RegisterSeller.mobno == mobno).count() == 0:
+            data = RegisterSeller(fname,regtype,gender,Address,enterdate,pincode,state,distict,tehsil,photoid,idno,mobno,altmobno,emailaddr,altemailaddr,bankname,holderbankname,bankaccno,cbankaccno)
+            db.session.add(data)
+            db.session.commit()
+            return "submitted"
+        else:
+            return 'already exist'
+        
+        return "successfull"
 
 
 
